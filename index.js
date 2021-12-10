a3e003cb7309e3c02d1ce852021b470fbb96cbc0
Omar Razi <startfresh87@gmail.com>
2021-12-09 22:27:46 -0500

    commit tree 81d803473cf6cc52476a891d8be0b301e7d8e752

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+
+function submitData(name, email) {
+  let configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type" : "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      "name": name,
+      "email": email
+    })
+  }
+
+  return fetch("http://localhost:3000/users",configObject)
+  .then(resp => resp.json())
+  .then(resp => {
+    document.body.innerHTML = resp.id
+  })
+  .catch(err => {
+    console.log(err)
+    document.body.innerHTML = err.message
+  })
+
+
+}
